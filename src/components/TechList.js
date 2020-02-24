5d5c8e2a4444a8d3df470ffea37357865a4156ee
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2020-02-24 20:25:44 -0300

    Learn about Props and Component lifecycle

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,12 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import TechItem from "./TechItem";
+import TechItem from './TechItem';
 
 class TechList extends Component {
   state = {
-    newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    newTech: '',
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -16,7 +31,7 @@ class TechList extends Component {
     e.preventDefault();
     this.setState({
       techs: [...this.state.techs, this.state.newTech],
-      newTech: ""
+      newTech: ''
     });
   };
 
@@ -38,11 +53,11 @@ class TechList extends Component {
             ))}
           </ul>
           <input
-            type="text"
+            type='text'
             onChange={this.handleInputChange}
             value={this.state.newTech}
           />
-          <button type="submit">Enviar</button>
+          <button type='submit'>Enviar</button>
         </form>
       </>
     );
